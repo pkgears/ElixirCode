9dee07a8227705c247db95f563f5d62b9955fc3e
Cesar Ortiz <pkgears@gmail.com>
2022-11-10 16:05:48 -0600

    Identicon: Create grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,16 +3,31 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
 
-  @doc """
-  Hello world.
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
 
-  ## Examples
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
 
-      iex> Identicon.hello()
-      :world
+    %Identicon.Image{hex: hex}
+  end
 
-  """
-  def hello do
-    :world
+  def pick_color(%Identicon.Image{ hex: [red, green, blue | _tail] } = image) do
+    %Identicon.Image{image | color: {red, green, blue}}
+  end
+
+  def build_grid(%Identicon.Image{ hex: hex } = image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 end
